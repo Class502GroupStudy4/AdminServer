a5dc4f9fa1f984d2d801b486caf453f61337ff11
jangjangji <sjjang41@naver.com>
2024-08-10 15:54:15 +0900

    파일부분 추가

--- src/main/java/org/g9project4/global/configs/FileConfig.java
+++ src/main/java/org/g9project4/global/configs/FileConfig.java
@@ -1,5 +1,6 @@
 package org.g9project4.global.configs;
 
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableConfigurationProperties(FileProperties.class)//FileProperties 에 있는 설정
 public class FileConfig implements WebMvcConfigurer {
     private final FileProperties properties;
+    private final EntityManager em;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
