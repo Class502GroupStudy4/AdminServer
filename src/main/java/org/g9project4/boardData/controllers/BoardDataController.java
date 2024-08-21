3a4ef624aa5341b8cb7657ba7769e9e2a029b607
jangjangji <sjjang41@naver.com>
2024-08-21 17:54:37 +0900

    게시물 자세히 보기 구현 완료

--- src/main/java/org/g9project4/boardData/controllers/BoardDataController.java
+++ src/main/java/org/g9project4/boardData/controllers/BoardDataController.java
@@ -3,6 +3,7 @@ package org.g9project4.boardData.controllers;
 import lombok.RequiredArgsConstructor;
 
 import org.g9project4.board.entities.Board;
+import org.g9project4.board.exceptions.BoardNotFoundException;
 import org.g9project4.board.repositories.BoardRepository;
 import org.g9project4.board.services.BoardConfigDeleteService;
 import org.g9project4.boardData.entities.BoardData;
@@ -26,6 +27,8 @@ public class BoardDataController {
     public final BoardRepository repository;
     public final BoardDataInfoService infoService;
     private final BoardConfigDeleteService configDeleteService;
+    private Board board; // 게시판 설정
+    private BoardData boardData; // 게시글 내용
 
 
     @GetMapping("/list/{bid}")
@@ -47,6 +50,14 @@ public class BoardDataController {
 
         model.addAttribute("script", "parent.location.reload();");
         return "common/_execute_script";
+    }
+
+    @GetMapping("/view/{seq}")
+    public String view(@PathVariable("seq") Long seq, Model model) {
+        boardData = infoService.get(seq);
+        model.addAttribute("boardData", boardData);
+
+        return "boardData/view";
     }
 
 }
