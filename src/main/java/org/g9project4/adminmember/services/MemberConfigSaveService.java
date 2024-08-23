002ce22101aab91f96125f442b76447fd73a719e
inseokLee999 <syt06137@naver.com>
2024-08-23 10:29:12 +0900

    관리자 선택 수정중

--- src/main/java/org/g9project4/adminmember/services/MemberConfigSaveService.java
+++ src/main/java/org/g9project4/adminmember/services/MemberConfigSaveService.java
@@ -1,26 +1,27 @@
 package org.g9project4.adminmember.services;
 
-import jakarta.ws.rs.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.g9project4.adminmember.controllers.RequestMember;
 import org.g9project4.global.Utils;
+import org.g9project4.global.exceptions.script.AlertException;
 import org.g9project4.member.constants.Authority;
-import org.g9project4.member.controllers.RequestJoin;
+import org.g9project4.member.entities.Authorities;
 import org.g9project4.member.entities.Member;
 import org.g9project4.member.repositories.MemberRepository;
-import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.g9project4.member.services.MemberSaveService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MemberConfigSaveService {
     private final MemberRepository memberRepository;
     private final Utils utils;
+    private final MemberSaveService memberSaveService;
 
-
-
-    public void save(RequestMember form){
+    public void save(RequestMember form) {
 
         String email = form.getEmail();
         Member member = memberRepository.findByEmail(email).orElseGet(Member::new);
@@ -33,4 +34,18 @@ public class MemberConfigSaveService {
         memberRepository.saveAndFlush(member);
     }
 
+    public void saveList(List<Integer> chks) {
+        if (chks == null || chks.isEmpty()) {
+            throw new AlertException("수정할 게시판을 선택하세요.", HttpStatus.BAD_REQUEST);
+        }
+
+        for (int chk : chks) {
+            String email = utils.getParam("email_" + chk);
+            Member member = memberRepository.findByEmail(email).orElse(null);
+            if (member == null) continue;
+            List<Authority> authorities = List.of(Authority.valueOf(utils.getParam("authority_"+chk)));
+            memberSaveService.save(member,authorities);
+        }
+
+    }
 }
