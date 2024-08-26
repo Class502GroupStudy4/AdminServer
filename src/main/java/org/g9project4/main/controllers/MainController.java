c31966cd5cc454a3bd649faefd242059eb4431e2
jangjangji <sjjang41@naver.com>
2024-08-26 20:07:07 +0900

    관리자 통계구현

--- src/main/java/org/g9project4/main/controllers/MainController.java
+++ src/main/java/org/g9project4/main/controllers/MainController.java
@@ -1,12 +1,17 @@
 package org.g9project4.main.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.g9project4.board.entities.Board;
+import org.g9project4.boardData.controllers.BoardDataSearch;
+import org.g9project4.boardData.entities.BoardData;
+import org.g9project4.boardData.repositories.BoardDataRepository;
+import org.g9project4.boardData.services.BoardDataInfoService;
+import org.g9project4.global.ListData;
 import org.g9project4.main.entities.VisitorCount;
 import org.g9project4.main.services.VisitorService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,12 +21,17 @@ import java.util.List;
 @RequestMapping("/")
 public class MainController {
     private final VisitorService visitorService;
+    public final BoardDataInfoService infoService;
 
 
 
 
     @GetMapping
-    public String index(Model model) {
+    public String index(@ModelAttribute BoardDataSearch search, Model model) {
+        String bid = "qna";
+        ListData<BoardData> data = infoService.getList(bid, search);
+        model.addAttribute("items", data.getItems());
+        model.addAttribute("pagination", data.getPagination());
         List<String> addCss = new ArrayList<>();
         addCss.add("main/style");
         List<String> addScript = new ArrayList<>();
@@ -36,4 +46,7 @@ public class MainController {
         model.addAttribute("stats", stats);
         return "main/index";
     }
+
+
+
 }
